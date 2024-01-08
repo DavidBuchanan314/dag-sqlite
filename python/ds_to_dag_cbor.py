97a4f671a9853db087f40f2b727374f44ce8ad75
David Buchanan <d@vidbuchanan.co.uk>
2024-01-08 16:44:03 +0000

    delete the bad version

--- python/ds_to_dag_cbor.py
+++ python/ds_to_dag_cbor.py
@@ -42,54 +42,6 @@ def calc_dag_cbor_varint_size(value: int):
 		return 5
 	return 9
 
-def ds_to_dag_cbor(stream: BinaryIO, cur: sqlite3.Cursor, obj_id: int) -> None:
-	stack = [(None, obj_id)]
-	while stack:
-		key_string, obj_id = stack.pop()
-		if key_string is not None: # special case for map keys
-			write_dag_cbor_varint(stream, CborMajorType.TEXT_STRING, len(key_string))
-			stream.write(key_string)
-		obj_type, intval, blobval = cur.execute("SELECT ds_obj_type, ds_obj_val_int, ds_obj_val_blob FROM ds_obj WHERE ds_obj_id=?", (obj_id,)).fetchone()
-		match DsTypes(obj_type):
-			case DsTypes.NULL:
-				write_dag_cbor_varint(stream, CborMajorType.SIMPLE, 22)
-			case DsTypes.BOOLEAN:
-				write_dag_cbor_varint(stream, CborMajorType.SIMPLE, 20 + intval)
-			case DsTypes.INTEGER:
-				write_dag_cbor_varint(stream, CborMajorType.INTEGER, intval & 0xffff_ffff_ffff_ffff)
-			case DsTypes.NEGATIVE_INTEGER:
-				write_dag_cbor_varint(stream, CborMajorType.NEGATIVE_INTEGER, intval & 0xffff_ffff_ffff_ffff)
-			case DsTypes.STRING:
-				write_dag_cbor_varint(stream, CborMajorType.TEXT_STRING, len(blobval))
-				stream.write(blobval)
-			case DsTypes.BYTES:
-				write_dag_cbor_varint(stream, CborMajorType.BYTE_STRING, len(blobval))
-				stream.write(blobval)
-			case DsTypes.LIST:
-				arrlen = cur.execute("SELECT COUNT(*) FROM ds_arr WHERE ds_arr_id=?", (intval,)).fetchone()[0]
-				write_dag_cbor_varint(stream, CborMajorType.ARRAY, arrlen)
-				arritems = [
-					row[0] for row in
-					cur.execute(
-						"SELECT ds_arr_val FROM ds_arr WHERE ds_arr_id=? ORDER BY ds_arr_idx",
-						(intval,)
-					).fetchall()
-				][::-1]
-				stack += [(None, x) for x in arritems]
-			case DsTypes.MAP:
-				maplen = cur.execute("SELECT COUNT(*) FROM ds_map WHERE ds_map_id=?", (intval,)).fetchone()[0]
-				write_dag_cbor_varint(stream, CborMajorType.MAP, maplen)
-				mapitems = cur.execute(
-					"SELECT ds_map_key, ds_map_val FROM ds_map WHERE ds_map_id=? ORDER BY length(ds_map_key), ds_map_key",
-					(intval,)
-				).fetchall()[::-1]
-				stack += mapitems
-
-			case _:
-				raise ValueError("unrecognised type")
-
-
-
 def ds_to_dag_cbor_recursionless(stream: BinaryIO, cur: sqlite3.Cursor, obj_id: int) -> None:
 	# temp hack to init the stack: (final version will grow ad hoc) (or maybe a preflight scan will already know?)
 	for i in range(100):
